4bc0c0298682f9e6c329c2b59404b7d1459ad94e
rbbri <robin.s.johnson@gmail.com>
2018-11-11 20:37:24 +0000

    Scores game of all ones

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,10 +2,17 @@ describe('Game', function () {
   require('../src/game')
 
   describe('score', function () {
+
     it('can score a gutter game', function () {
       var game = new Game()
-      for (i = 0; i < 20; i++) { game.scoreFrame(0, 0) }
+      for (i = 0; i < 10; i++) { game.scoreFrame(0, 0) }
       expect(game.totalScore()).toEqual(0)
+    })
+
+    it('can score a game of all ones', function () {
+      var game = new Game()
+      for (i = 0; i < 10; i++) { game.scoreFrame(1, 1) }
+      expect(game.totalScore()).toEqual(20)
     })
   })
 })
--- src/game.js
+++ src/game.js
@@ -1,10 +1,13 @@
 Game = function () {
   this.currentFrame = 0
+  this.score = 0
 }
 
-Game.prototype.scoreFrame = function () {
+Game.prototype.scoreFrame = function (roll_one, roll_two) {
+  this.score += roll_one + roll_two
+
 }
 
 Game.prototype.totalScore = function () {
-  return 0
+  return this.score
 }
